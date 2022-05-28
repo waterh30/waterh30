cdc42667e255624f7d8e21f82a97f551305f9cc7
Steven Zhao <waterh30@gmail.com>
2022-05-28 13:15:51 -0500

    added sg

--- tf-study/sample-code/ec2/main.tf
+++ tf-study/sample-code/ec2/main.tf
@@ -19,6 +19,14 @@
 resource "aws_instance" "myInstance" {
     ami = data.aws_ami.std_ami.id
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.web-server.id]
+    user_data = <<-EOF
+      #!/bin/bash
+      yum -y install httpd
+      echo "<h1>Hello, World<h1>" > /var/www/html/index.html
+      sudo systemctl enable http
+      sudo systemctl start http
+      EOF
 
     tags = {
         Name = "myInstance"
@@ -26,3 +34,17 @@ resource "aws_instance" "myInstance" {
 
 }
 
+resource "aws_security_group" "web-server" {
+    name = "myInstance"
+    ingress {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      description = "web sg"
+      from_port = 80
+      protocol = "tcp"
+      to_port = 80
+    } 
+}
+
+output "DNS" {
+    value = aws_instance.myInstance.public_dns
+}
